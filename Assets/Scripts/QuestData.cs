7c8bcae45f4d4b04b37c6c35e9be72fd6b7b5e44
jaeho <>
2026-07-07 16:54:30 +0900

    [feat] QuestData / Model 작성

---
+++ Assets/Scripts/QuestData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class QuestData : GameDataBase
+{
+    public string QuestType;
+    public string Description;
+    public string TargetId;
+    public int TargetCount;
+}
