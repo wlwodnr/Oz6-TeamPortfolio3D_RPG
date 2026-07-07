fc0e0dd964378880db0a258a4f4fe313136bcfd4
wlwodnr <145469443+wlwodnr@users.noreply.github.com>
2026-07-07 17:05:31 +0900

    [feat] EnemyEntity 기본 틀 작성
    생성된 EnemyEntity의 초기화 부분을 담당함. 오브젝트 ID값을 받아와 확인할 수 있다.
    하지만 초기화 부분이나, 데이터 부분은 추가 작업이 필요하다

---
+++ Assets/Scripts/Enemy/EnemyEntity.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EnemyEntity : MonoBehaviour, IGameObjectEntity
+{
+    [SerializeField] private int _instanceId = -1;
+
+    [SerializeField] private string _enemyDataId;
+
+    public int InstanceId
+    {
+        get { return _instanceId; }
+    }
+
+    public void InitEntity(int instanceId, string dataId)
+    {
+        _instanceId = instanceId;
+        _enemyDataId = dataId;
+
+        gameObject.SetActive(true);
+
+        //JU ToDo 데이터 드리븐으로 EnemyData를 갖고오는 작업을 해야한다.
+        Debug.Log($"Enemy 초기화 완료. InstanceId: {_instanceId}, EnemyDataId: {_enemyDataId}");
+    }
+
+    public void ResetEntity()
+    {
+        Debug.Log($"Enemy 리셋. InstanceId: {_instanceId}");
+
+        _instanceId = -1;
+        _enemyDataId = string.Empty;
+
+        //JU ToDo 나중에 Stat 등이 완성되면 HP, 상태, 타겟, 에니메이션, NaveMeshAgent 등을 초기화 해야함
+    }
+}
