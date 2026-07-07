b193a0b247b7612d71cbff9e62ba1643c20f5c37
yeoil-1 <2001yeoil@gmail.com>
2026-07-07 15:16:19 +0900

    [feat] MonsterData 작성

---
+++ Assets/Scripts/DataDriven/MonsterData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MonsterData
+{
+    public string Name;
+    public string Description;
+    public string PrefabPath;
+    public string IconPath;
+
+    public int BaseHP;
+    public int BaseAttack;
+    public float MoveSpeed;
+
+    public float DetectRange;
+    public float AttackRange;
+    public float SpawnLimitRange;
+    public float PatrolRange;        // 배회 반경
+}
