18633e985bd029961d96da591d42c1a151b9c9c9
wlwodnr <145469443+wlwodnr@users.noreply.github.com>
2026-07-07 13:20:22 +0900

    [feat] GameManager 기본 틀 만들기
    싱글턴 구조로 만들고, 각 상태를 나열해놓았다.
    추가적으로 각 상태에 따른 로직을 추가할 예정

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private GameState _currentState = GameState.None;
+
+    public GameState CurrentState
+    {
+        get { return _currentState; }
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log($"중복된 GameManager가 발견되어 파괴합니다: {gameObject.name}");
+            Destroy(gameObject);
+            return;
+        }
+        ChangeGameState(GameState.Boot);
+        
+    }
+
+    private void Start()
+    {
+        ChangeGameState(GameState.Title);
+    }
+
+    public void ChangeGameState(GameState nextState)
+    {
+        if(_currentState == nextState)
+        {
+            return;
+        }
+
+        GameState prevState = _currentState;
+        _currentState = nextState;
+
+        Debug.Log($"게임 상태 변경: {prevState} 에서 {CurrentState}로 변경됩니다.");
+    }
+
+    private void HandleGameStateChanged(GameState prevState, GameState currentState)
+    {
+
+    }
+}
