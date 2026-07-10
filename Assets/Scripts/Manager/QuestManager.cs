1acccfedfbe0283da1bf91235e8026e7041a3f9b
jaeho <>
2026-07-10 13:20:46 +0900

    [feat] 퀘스트 상태별 로그와 이벤트

--- Assets/Scripts/Manager/QuestManager.cs
+++ Assets/Scripts/Manager/QuestManager.cs
@@ -8,7 +8,9 @@ public class QuestManager : MonoBehaviour
     // 현재 진행중인 퀘스트목록
     private Dictionary<string, QuestModel> _activeQuests = new Dictionary<string, QuestModel>();
 
-    // MVVM UI 연동 이벤트 작성할 예정
+    public delegate void QuestStateChangedHandler(string questId);
+    public event QuestStateChangedHandler OnQuestStateChanged;
+
 
     private void Awake()
     {
@@ -25,11 +27,49 @@ public class QuestManager : MonoBehaviour
 
     public void AcceptQuest(string questId)
     {
+        if (_activeQuests.ContainsKey(questId))
+        {
+            Debug.LogWarning($"이미 수락한 퀘스트입니다 quest ID: {questId}");
+            return;
+        }
 
+        QuestData data = GameDataManager.Instance.GetQuestData(questId);
+
+        if(data == null)
+        {
+            Debug.LogWarning($"QuestData를 찾을수 없습니다 QuestId : {questId}");
+            return;
+        }
     }
 
     public void CompleteQuest(string questId)
     {
+        if (!_activeQuests.TryGetValue(questId, out var model))
+        {
+            Debug.LogWarning($"진행중인 퀘스트 목록에 없습니다 QUestId: {questId}");
+            return;
+        }
 
+        if (!model.IsCompleted)
+        {
+            Debug.LogWarning($"아직 퀘스트 조건을 만족하지 못했습니다 QuestId: {questId}");
+            return;
+        }
+
+        if (model.IsRewardReceived)
+        {
+            Debug.LogWarning($"이미 보상이 지급된 퀘스트입니다 QuestId: {questId}");
+            return;
+        }
+    }
+
+    public QuestModel GetActiveQuestModel(string questId)
+    {
+        if (_activeQuests.TryGetValue(questId, out var model))
+        {
+            return model;
+        }
+
+        return null;
     }
 }
