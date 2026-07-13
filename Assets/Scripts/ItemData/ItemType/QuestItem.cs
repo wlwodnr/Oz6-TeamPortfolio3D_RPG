b31dee77a6ce602e9277a051a0989961246fb2ba
rlagotqks081 <khbkss77@naver.com>
2026-07-13 14:44:45 +0900

    [fix] 깃 저장용

---
+++ Assets/Scripts/ItemData/ItemType/QuestItem.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "QuestItem", menuName = "ItemData/QuestItem")]
+public class QuestItem : ItemBase
+{
+    public string QuestID; // 이것도 없앨수도?
+}
