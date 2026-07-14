80d691432323057417a5ae7a9787e76bc83f996f
Kim donguk <baby00du20@gmail.com>
2026-07-14 18:44:31 +0900

    특정 레벨 도달시 강화 스킬 해금, 강화 스킬 습득 여부 체크 함수 추가, 스킬데이터 갱신

--- Assets/Scripts/Skill/SkillData.cs
+++ Assets/Scripts/Skill/SkillData.cs
@@ -17,8 +17,11 @@ public class SkillData : GameDataBase
     public string Name;
     public string Description;
     public string IconPath;
+    public string SkillType;
+    public string TargetMode;
 
     public float DamageMultiplier;
     public List<float> MultiHitPercentList;
 
+    public int RequiredLevel;
 }
--- Assets/Scripts/Skill/SkillModel.cs
+++ Assets/Scripts/Skill/SkillModel.cs
@@ -12,11 +12,51 @@ public class SkillModel
         IsBossModeActive = !IsBossModeActive;
     }
 
+    public bool TryLearnSkill(SkillData skillData, PlayerInfo playerInfo)
+    {
+        if (skillData == null || playerInfo == null)
+        {
+            return false;
+        }
+
+        if (playerInfo.CurLevel >= skillData.RequiredLevel && playerInfo.SkillPoint >= 1)
+        {
+            playerInfo.SkillPoint -= 1;
+            UnlockedSkillIds.Add(skillData.Id.ToString());
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool CheckSkillAdvance(string targetSkillId)
+    {
+        return UnlockedSkillIds.Contains(targetSkillId);
+    }
+
     public string GetCurrentSkillId(string baseSkillId)
     {
-        if (baseSkillId == "Active_01" && IsBossModeActive == true)
+        if (baseSkillId == "Active_H_01" || baseSkillId == "Active_B_01")
         {
-            return "Active_02";
+            bool hasAdvancedSkill = UnlockedSkillIds.Contains("Active_H_02");
+
+            if (hasAdvancedSkill)
+            {
+                if (!IsBossModeActive == true)
+                {
+                    return "Active_B_02";
+                }
+
+                return "Active_H_02";
+            }
+
+            if (IsBossModeActive == true)
+            {
+                return "Active_B_01";
+            }
+
+            return "Active_H_01";
         }
 
         return baseSkillId;
