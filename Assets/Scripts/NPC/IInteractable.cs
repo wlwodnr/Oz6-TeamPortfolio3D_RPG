6f26c1bb4c3ad19f04ec6b06082d6b00dbbc2138
jaeho <>
2026-07-18 13:01:22 +0900

    [feat] 퀘스트 NPC 구현 준비

---
+++ Assets/Scripts/NPC/IInteractable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public interface IInteractable
+{
+    // 한글 테스트
+    bool CanInteract { get; }
+
+    void Interact();
+}
---
+++ Assets/Scripts/NPC/NPCQuest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class NPCQuest : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string QuestId;
+
+    public bool CanInteract
+    {
+        get
+        {
+            return true;
+        }
+    }
+
+    public void Interact()
+    {
+
+    }
+}
