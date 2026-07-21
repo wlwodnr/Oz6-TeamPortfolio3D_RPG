1a9f8e0dbc45c7005b9d8807e397485f7a5406ba
jaeho <>
2026-07-21 16:45:58 +0900

    [feat] QuestUI 작성 / QuestData 수정

--- Assets/Scripts/UI/QuestUI.cs
+++ Assets/Scripts/UI/QuestUI.cs
@@ -7,10 +7,18 @@ public class QuestUI : UIBase
     [SerializeField] private Text Text_QuestDescription;
     [SerializeField] private Text Text_Progress;
 
+    //연출용 테스트라고 보셔도 됩니다 
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private GameObject Effect_QuestComplete;
+
+    private string _trackedQuestId;
+
     private void OnEnable()
     {
         if (QuestManager.Instance != null)
             QuestManager.Instance.OnQuestStateChanged += OnQuestStateChanged;
+
+        RefreshFromCurrentState();
     }
 
     private void OnDisable()
@@ -21,16 +29,57 @@ public class QuestUI : UIBase
 
     private void OnQuestStateChanged(string questId)
     {
+        RefreshFromCurrentState();
+    }
 
+    private void RefreshFromCurrentState()
+    {
+        if (QuestManager.Instance == null) return;
+
+        if(QuestManager.Instance.TryGetDisplayQuest(out string questId, out QuestModel model) == false)
+        {
+            Hide();
+            return;
+        }
+
+        QuestData questData = GameDataManager.Instance.GetQuestData(questId);
+        if (questData == null)
+        {
+            Hide();
+            return;
+        }
+
+        _trackedQuestId = questId;
+        Show();
+
+        Text_QuestTitle.text = questData.QuestTitle;
+        Text_QuestDescription.text = questData.Description;
+        Text_Progress.text = $"({model.CurrentCount} / {questData.TargetCount})";
+
+        
+    }
+
+    // 연출테스트용
+    private void SetCompleteEffect(bool isComplete)
+    {
+        if(Effect_QuestComplete != null)
+            Effect_QuestComplete.SetActive(isComplete);
+
+        Text_Progress.color = isComplete ? Color.yellow : Color.white;
     }
 
     private void Show()
     {
-
+        gameObject.SetActive(true);
+        if(canvasGroup != null)
+            canvasGroup.alpha = 1;
     }
 
     private void Hide()
     {
-
+        if (canvasGroup != null)
+            canvasGroup.alpha = 0;
+        else
+            gameObject.SetActive(false);
     }
 }
