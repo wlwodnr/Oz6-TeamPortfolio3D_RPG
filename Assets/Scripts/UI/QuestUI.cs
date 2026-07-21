fb84846250929791a490778f014c36d1332042a2
jaeho <>
2026-07-21 15:50:21 +0900

    [feat] QuestUI 중간저장

--- Assets/Scripts/UI/QuestUI.cs
+++ Assets/Scripts/UI/QuestUI.cs
@@ -1,6 +1,36 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestUI : UIBase
 {
-    
+    [SerializeField] private Text Text_QuestTitle;
+    [SerializeField] private Text Text_QuestDescription;
+    [SerializeField] private Text Text_Progress;
+
+    private void OnEnable()
+    {
+        if (QuestManager.Instance != null)
+            QuestManager.Instance.OnQuestStateChanged += OnQuestStateChanged;
+    }
+
+    private void OnDisable()
+    {
+        if(QuestManager.Instance != null)
+            QuestManager.Instance.OnQuestStateChanged -= OnQuestStateChanged;
+    }
+
+    private void OnQuestStateChanged(string questId)
+    {
+
+    }
+
+    private void Show()
+    {
+
+    }
+
+    private void Hide()
+    {
+
+    }
 }
