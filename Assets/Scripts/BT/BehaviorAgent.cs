2e9b534d93277bec78fb416bbe09bc72f19b4ded
yeoil-1 <2001yeoil@gmail.com>
2026-07-22 18:59:43 +0900

    [fix] 적이 플레이어의 인스턴스 아이디를 가져오도록 수정

---
+++ Assets/Scripts/BT/BehaviorAgent.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Unity.Behavior;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+public class BehaviorAgent : MonoBehaviour
+{
+    [SerializeField] private BehaviorGraphAgent BehaviorAgent_Self;
+    [SerializeField] private List<GameObject> PatrolSpotGameObjectList;
+
+
+    private void Start()
+    {
+        if (PatrolSpotGameObjectList != null && PatrolSpotGameObjectList.Count > 0)
+        {
+            BehaviorAgent_Self.SetVariableValue("PatrolSpotList", PatrolSpotGameObjectList);
+        }
+
+        
+        
+        int playerInstanceId = GameObjectManager.Instance.PlayerInstanceId;
+
+        GameObject playerObject = GameObjectManager.Instance.GetGameObjectCanBeNull(playerInstanceId);
+
+        if (playerObject != null )
+        {
+            BehaviorAgent_Self.SetVariableValue("Target",playerObject);
+        }
+        
+    }
+
+}
