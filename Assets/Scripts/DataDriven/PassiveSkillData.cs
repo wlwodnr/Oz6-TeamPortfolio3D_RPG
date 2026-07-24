6c20572de4e45642ea03324f8ffc53c2ce053560
Kim donguk <baby00du20@gmail.com>
2026-07-24 14:38:40 +0900

    value 내용 statmodifier와 통합, value를 모디파이어 객체로 변환

--- Assets/Scripts/DataDriven/PassiveSkillData.cs
+++ Assets/Scripts/DataDriven/PassiveSkillData.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 [System.Serializable]
 public class PassiveSkillData : GameDataBase
@@ -11,11 +13,43 @@ public class PassiveSkillData : GameDataBase
     public string[] RequiredSkill;
     public string[] PassiveStatType;
     public float[] PassiveValue;
-    public float[] PassiveModifier;
+    public string[] ModifierType;
     public float Duration;
     public float TimeInterval;
     public string TriggerCondition;
     public float TriggerValue;
     public float CoolDown;
     public int MaxStack;
+
+    public List<StatModifier> GetStatModifiers()
+    {
+        List<StatModifier> modifiers = new List<StatModifier>();
+
+        if (PassiveStatType == null || PassiveStatType.Length == 0)
+            return modifiers;
+
+        for (int i = 0; i < PassiveStatType.Length; i++)
+        {
+            if (!Enum.TryParse<StatType>(PassiveStatType[i], true, out StatType statType))
+                continue;
+
+            ModifierType modType = global::ModifierType.Flat;
+            if (ModifierType != null && i < ModifierType.Length)
+            {
+                Enum.TryParse<ModifierType>(ModifierType[i], true, out modType);
+            }
+
+            if (PassiveValue != null && i < PassiveValue.Length && Mathf.Abs(PassiveValue[i]) > 0.0001f)
+            {
+                modifiers.Add(new StatModifier
+                {
+                    Type = statType,
+                    ModType = modType,
+                    Value = PassiveValue[i]
+                });
+            }
+        }
+
+        return modifiers;
+    }
 }
