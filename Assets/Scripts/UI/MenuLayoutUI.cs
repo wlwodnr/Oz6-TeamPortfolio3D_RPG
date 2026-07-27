b8ad375f8f4d0415c86b68b8e230c70418787bdf
조윤상 <jogreenx21@gmail.com>
2026-07-27 17:24:18 +0900

    [fix] 커서 문제 임시 해결

---
+++ Assets/Scripts/UI/MenuLayoutUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class MenuLayoutUI : UIBase
+{
+    [Header("Buttons")]
+    [SerializeField] private UIButton Btn_Inventory;
+    [SerializeField] private UIButton Btn_SettingPopup;
+    [SerializeField] private UIButton Btn_CloseSelf;
+    private void OnEnable()
+    {
+        Btn_Inventory.BindOnClickButtonEvent(OnClickOpenInventory);
+        Btn_SettingPopup.BindOnClickButtonEvent(OnClickOpenSettingPopup);
+        Btn_CloseSelf.BindOnClickButtonEvent(OnclickCloseSelf);
+    }
+
+    private void OnClickOpenSettingPopup()
+    {
+        UIManager.Instance.OpenSettingUI();
+    }
+
+    private void OnClickOpenInventory()
+    {
+        UIManager.Instance.OpenContentUI(UIType.InventoryUI);
+    }
+    private void OnclickCloseSelf()
+    {
+        UIManager.Instance.CloseUI(UIRootType.MainUI, UIType.MenuLayoutUI);
+        InputManager.Instance.SetCursorAndInputState(false);
+    }
+}
