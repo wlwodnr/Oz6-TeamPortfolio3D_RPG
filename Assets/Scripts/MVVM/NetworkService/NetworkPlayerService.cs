9428130f2ae027f066e5b44dbb1187178c987e07
wlwodnr <145469443+wlwodnr@users.noreply.github.com>
2026-07-27 09:48:08 +0900

    [feat] 플레이어 레벨업 밑 스텟

--- Assets/Scripts/MVVM/NetworkService/NetworkPlayerService.cs
+++ Assets/Scripts/MVVM/NetworkService/NetworkPlayerService.cs
@@ -67,12 +67,29 @@ public class NetworkPlayerService
 
     public void RequestGiveExpToLocalPlayer(float exp)
     {
-        _playerModel.Info.TotalExp += exp;
+        if (exp <= 0f || float.IsNaN(exp) || float.IsInfinity(exp))
+        {
+            Debug.LogWarning($"지급할 경험치는 0보다 커야 합니다. Experience: {exp}");
+            return;
+        }
+
+        GetLocalPlayerModel().AddExperience(exp);
+    }
+
+    public void RequestGiveGoldToLocalPlayer(int gold)
+    {
+        if (gold <= 0)
+        {
+            Debug.LogWarning($"지급할 골드는 0보다 커야 합니다. Gold: {gold}");
+            return;
+        }
+
+        GetLocalPlayerModel().AddGold(gold);
     }
 
     public void RequestChangePlayerLevel(int level)
     {
-        _playerModel.Info.CurLevel = level;
+        GetLocalPlayerModel().Info.CurLevel = level;
     }
 
     public void RequestChangePlayerName(string newName)
