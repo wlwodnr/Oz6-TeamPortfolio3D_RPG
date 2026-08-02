d6001c6c9e35ee3f4446555f696a5068fb757375
wlwodnr <145469443+wlwodnr@users.noreply.github.com>
2026-08-02 20:31:20 +0900

    [feat] 스폰 스팟에 하나의 오브젝트 스폰을 위한 준비

---
+++ Assets/Scripts/GameObject/SingleSpawnSpotExtension.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class SingleSpawnSpotExtension
+{
+    [Header("단일 생성 오브젝트")]
+    [SerializeField] private GameObject Prefab_SpawnObject;
+
+    [Header("단일 생성 데이터")]
+    [SerializeField] private string _dataId;
+
+    public int RequestSpawn(SpawnSpot ownerSpawnSpot, Transform spawnPoint)
+    {
+        if (ownerSpawnSpot == null)
+        {
+            Debug.LogWarning("SingleSpawnSpotExtension: Owner SpawnSpot이 없어 오브젝트를 생성할 수 없습니다.");
+            return -1;
+        }
+        if (Prefab_SpawnObject == null)
+        {
+            Debug.LogWarning($"SingleSpawnSpotExtension: [{ownerSpawnSpot.gameObject.name}] 생성할 Prefab이 등록되지 않았습니다.", ownerSpawnSpot);
+            return -1;
+        }
+        if (GameObjectManager.Instance == null)
+        {
+            Debug.LogWarning($"SingleSpawnSpotExtension: [{ownerSpawnSpot.gameObject.name}] GameObjectManager가 없어 오브젝트를 생성할 수 없습니다.", ownerSpawnSpot);
+            return -1;
+        }
+
+        Transform targetSpawnPoint = spawnPoint == null ? ownerSpawnSpot.transform : spawnPoint;
+
+        return GameObjectManager.Instance.RequestSpawnGameObject(Prefab_SpawnObject, targetSpawnPoint.position, targetSpawnPoint.rotation, _dataId, ownerSpawnSpot);
+    }
+}
